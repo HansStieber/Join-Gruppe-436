7daf3651ec7aa59c66faf0949862a59f27c8aa72
HansStieber <hans.stieber@posteo.de>
2023-01-20 16:41:03 +0100

    assign dropdown menu added

--- dropdown_category.js
+++ dropdown_category.js
@@ -1,47 +1,132 @@
 let categoryOptions = [
     {
-        'id': '0',
-        'name': 'New Category'
-    },
-    {
-        'id': '1',
         'name': 'General Topic'
     },
+    {
+        'name': 'example'
+    },
+    {
+        'name': 'example'
+    }
 ];
 
+
+let contacts = [
+    {
+        'name': 'Hans'
+    },
+    {
+        'name': 'Sasha'
+    }
+]
+
+
+/*----------- LOADING OPTIONS OF DROPDOWN SELECTION MENUS -----------*/
 function render() {
     loadCategoryOptions();
+    loadAssignmentOptions();
 }
+
 
 function loadCategoryOptions() {
     for (let i = 0; i < categoryOptions.length; i++) {
         const option = categoryOptions[i];
-        renderCategoryOptions(option);
+        if (i == categoryOptions.length - 1) {
+            renderLastCategoryOption(option, i);
+        } else {
+            renderCategoryOptions(option, i);
+        }
     }
 }
 
+
+function loadAssignmentOptions() {
+    for (let i = 0; i < contacts.length; i++) {
+        const option = contacts[i];
+        renderAssignmentOptions(option, i);
+    }
+}
+
+
+/*----------- OPEN DROPDOWN MENU FOR CATEGORY SELECTION -----------*/
 function openDropdownCategory() {
     for (let i = 0; i < categoryOptions.length; i++) {
         const option = categoryOptions[i];
-        document.getElementById(option['id']).classList.remove('d-none');
+        document.getElementById('c-option' + i).classList.remove('d-none');
     }
+    let createNewCategory = document.getElementById('create-new-category');
+    createNewCategory.classList.remove('d-none');
     let selectContainer = document.getElementById('select-container');
-    selectContainer.setAttribute('onclick','closeDropdownCategory()');
+    selectContainer.setAttribute('onclick', 'closeDropdownCategory()');
 }
 
+
+/*----------- CLOSE DROPDOWN MENU FOR CATEGORY SELECTION -----------*/
 function closeDropdownCategory() {
     for (let i = 0; i < categoryOptions.length; i++) {
         const option = categoryOptions[i];
-        document.getElementById(option['id']).classList.add('d-none');
+        document.getElementById('c-option' + i).classList.add('d-none');
     }
+    let createNewCategory = document.getElementById('create-new-category');
+    createNewCategory.classList.add('d-none');
     let selectContainer = document.getElementById('select-container');
-    selectContainer.setAttribute('onclick','openDropdownCategory()');
+    selectContainer.setAttribute('onclick', 'openDropdownCategory()');
 }
 
-function renderCategoryOptions(option) {
+
+/*----------- CREATE NEW CATEGORY FOR SELECTION -----------*/
+function createNewCategory() {
+    openInputField();
+    closeDropdownCategory();
+}
+
+
+function openInputField() {
+    let newCategoryInput = document.getElementById('new-category');
+    newCategoryInput.classList.remove('d-none');
+
+    let newCategoryContainer = document.getElementById('new-category-container');
+    newCategoryContainer.classList.remove('d-none');
+
+    let optionsContainer = document.getElementById('options-container');
+    optionsContainer.classList.add('d-none');
+}
+
+
+/*----------- OPEN DROPDOWN MENU FOR CATEGORY SELECTION -----------*/
+
+
+
+
+
+/*----------- TEMPLATES -----------*/
+
+/*----------- TEMPLATES FOR CATEGORY SELECTION -----------*/
+function renderCategoryOptions(option, i) {
     let optionsContainer = document.getElementById('options-container');
     optionsContainer.innerHTML += `
-    <div id="${option['id']}" class="option d-none">
+    <div id="${'c-option' + i}" class="option d-none selectable">
+        <span>${option['name']}</span>
+    </div>
+`;
+}
+
+
+function renderLastCategoryOption(option, i) {
+    let optionsContainer = document.getElementById('options-container');
+    optionsContainer.innerHTML += `
+    <div id="${'c-option' + i}" class="option d-none selectable last-option">
+        <span>${option['name']}</span>
+    </div>
+    `;
+}
+
+
+/*----------- TEMPLATES FOR ASSIGNING CONTACT -----------*/
+function renderAssignmentOptions(option, i) {
+    let optionsContainer = document.getElementById('options-container');
+    optionsContainer.innerHTML += `
+    <div id="${'c-option' + i}" class="option d-none selectable">
         <span>${option['name']}</span>
     </div>
 `;
