be1e2f64e21141958b705a3dcbbded3c38c8ff1d
PierreRudolph <Plettner@hotmail.de>
2023-01-24 16:19:43 +0100

    clean code add contact big card

--- contacts.js
+++ contacts.js
@@ -2,21 +2,31 @@ class Person {
     firstName;
     lastName;
     email;
-    constructor(firstName, lastName, email) {
+    color;
+    password;
+    constructor(firstName, lastName, email, password, color) {
         this.firstName = upperCaseFirstLetter(firstName);
         this.lastName = upperCaseFirstLetter(lastName);
         this.email = email;
+        if (color) {
+            this.color = color;
+        } else {
+            this.color = generateRandomColor();
+        }
+        this.password = password;
     }
+
+
 }
 
+
 class Contact extends Person {
-    password;
+
     phone;
 
-    constructor(firstName, lastName, phone, email, password) {
-        super(firstName, lastName, email);
+    constructor(firstName, lastName, phone, email, password, color) {
+        super(firstName, lastName, email, password, color);
         this.phone = phone;
-        this.password = password;
     }
 
     call() {
@@ -24,15 +34,15 @@ class Contact extends Person {
     }
 }
 
-let abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+let abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let contacts = [
-    new Contact('Hans', 'Stieber', '0151854715', 'stieber@mail.de', 'P4SsW0rTeins2drei'),
-    new Contact('Daniela', 'Scholz', '0151854715', 'scholz@mail.de', 'P4SsW0rTeins2drei'),
-    new Contact('Sasha', 'Seslija', '0151854715', 'seslija@mail.de', 'P4SsW0rTeins2drei'),
-    new Contact('zans', 'Petersson', '0151854715', 'petersson@mail.de', 'P4SsW0rTeins2drei'),
-    new Contact('Fans', 'Petersson', '0151854715', 'petersson@mail.de', 'P4SsW0rTeins2drei'),
-    new Contact('Jans', 'Petersson', '0151854715', 'petersson@mail.de', 'P4SsW0rTeins2drei'),
+    new Contact('Hans', 'Stieber', '01518547387', 'stieber@mail.de', 'P4SsW0rTeins2drei', 'blue'),
+    new Contact('Daniela', 'Scholz', '01518543875', 'scholz@mail.de', 'P4SsW0rTeins2drei', 'purple'),
+    new Contact('Sasha', 'Seslija', '01518543875', 'seslija@mail.de', 'P4SsW0rTeins2drei', 'green'),
+    new Contact('Pierre', 'Lettner', '0151854715', 'lettner@mail.de', 'P4SsW0rTeins2drei,', 'red'),
+    new Contact('Fans', 'Petersson', '0151854715', 'petersson@mail.de', 'P4SsW0rTeins2drei', 'cyan'),
+    new Contact('Jans', 'Petersson', '01518543875', 'petersson@mail.de', 'P4SsW0rTeins2drei', 'grey'),
     new Contact('Kans', 'Petersson', '0151854715', 'petersson@mail.de', 'P4SsW0rTeins2drei'),
     new Contact('Qans', 'Petersson', '0151854715', 'petersson@mail.de', 'P4SsW0rTeins2drei'),
     new Contact('Xans', 'Petersson', '0151854715', 'petersson@mail.de', 'P4SsW0rTeins2drei'),
@@ -43,9 +53,6 @@ let contacts = [
     new Contact('Uans', 'Petersson', '0151854715', 'petersson@mail.de', 'P4SsW0rTeins2drei')
 ];
 
-function upperCaseFirstLetter(name) {
-    return name.charAt(0).toUpperCase() + name.slice(1);
-}
 
 function clearCards() {
     for (let i = 0; i < abc.length; i++) {
@@ -55,6 +62,7 @@ function clearCards() {
     }
 }
 
+
 function addCardsDivMainDNone() {
     for (let i = 0; i < abc.length; i++) {
         const letter = abc[i];
@@ -63,35 +71,98 @@ function addCardsDivMainDNone() {
     }
 }
 
+
 function renderContacts() {
     clearCards();
     addCardsDivMainDNone();
     setTimeout(function () {
         for (let i = 0; i < contacts.length; i++) {
-
             const contact = contacts[i];
-            const firstNameFirstLetter = contact.firstName.slice(0, 1);
-            const lastNameFirstLetter = contact.lastName.slice(0, 1);
             if (contact) {
-                const cardsMainLetter = document.getElementById(`cards-main-${firstNameFirstLetter}`);
+                const { cardsMainLetter, cardsDivLetter } = getContactInfo(contact);
                 cardsMainLetter.classList.remove('d-none');
-                const cardsDivLetter = document.getElementById(`cards-div-${firstNameFirstLetter}`);
-                cardsDivLetter.innerHTML +=/*html*/`
-            <div class="contact-card">
-                <div class="contact-initials-small">
-                    <span>${firstNameFirstLetter}</span>
-                    <span>${lastNameFirstLetter}</span>
-                </div>
-                <div class="contact-card-info">
-                    <div>
-                        <span>${contact.firstName}</span>
-                        <span>${contact.lastName}</span>
-                    </div>
-                    <a href="#" style="text-decoration-line:none;">${contact.email}</a>
-                </div>
-            </div>
-        `;
+                cardsDivLetter.innerHTML += getCardInnerHTML(contact, i);
             }
         }
-    }, 200)
+    }, 500)
+}
+
+
+function getContactInfo(contact) {
+    ini1 = contact.firstName.slice(0, 1);
+    ini2 = contact.lastName.slice(0, 1);
+    return {
+        email: contact.email,
+        phone: contact.phone,
+        initials1: ini1,
+        initials2: ini2,
+        bgColor: contact.color,
+        firstName: contact.firstName,
+        lastName: contact.lastName,
+        cardsMainLetter: document.getElementById(`cards-main-${ini1}`),
+        cardsDivLetter: document.getElementById(`cards-div-${ini1}`)
+    }
+}
+
+
+function renderBigCard(indexNum) {
+    let contact = contacts[indexNum];
+
+    const { initials1, initials2, bgColor, email,
+        phone, firstName, lastName } = getContactInfo(contact);
+
+    const { initialsSpan, firstNameSpan, lastNameSpan,
+        emailLink, phoneSpan, bigCardDiv, initialsDiv } = getBigCardElements();
+
+    initialsSpan.innerHTML = initials1 + initials2;;
+    firstNameSpan.innerHTML = firstName;
+    lastNameSpan.innerHTML = lastName;
+    emailLink.innerHTML = email;
+    phoneSpan.innerHTML = phone;
+
+    initialsDiv.style = `background-color:${bgColor}`;
+    bigCardDiv.classList.remove('d-none');
+}
+
+
+function getBigCardElements() {
+    return {
+        initialsSpan: document.getElementById('big-card-initials'),
+        firstNameSpan: document.getElementById('big-card-firstname'),
+        lastNameSpan: document.getElementById('big-card-lastname'),
+        emailLink: document.getElementById('big-card-email'),
+        phoneSpan: document.getElementById('big-card-phone'),
+        bigCardDiv: document.getElementById('big-card'),
+        initialsDiv: document.getElementById('big-card-initials-bg')
+    }
+}
+
+
+function getCardInnerHTML(contact, i) {
+    const { initials1, initials2, bgColor, firstName, lastName } = getContactInfo(contact);
+    return /*html*/`
+    <div onclick="renderBigCard(${i})" class="contact-card">
+        <div class="contact-initials-small" style="background-color:${bgColor}">
+            <span>${initials1}</span>
+            <span>${initials2}</span>
+        </div>
+        <div class="contact-card-info">
+            <div>
+                <span>${firstName}</span>
+                <span>${lastName}</span>
+            </div>
+            <a href="#" style="text-decoration-line:none;">${contact.email}</a>
+        </div>
+    </div>
+`;
+}
+
+
+function upperCaseFirstLetter(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+}
+
+
+function generateRandomColor() {
+    return '#' + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6)
 }
