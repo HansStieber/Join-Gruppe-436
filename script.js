aa841193086f1d00fa4a21c34f1d1aff3885aa52
cyberwerkstatt <seslija.sasha@gmail.com>
2023-01-26 22:51:54 +0100

    reset password updated

--- script.js
+++ script.js
@@ -15,3 +15,11 @@ function sendMail(){
 function guestLogin() {
     window.location.href = './summary.html'
 }
+
+
+function forgotPassword(){
+    let forgotContainer = document.getElementById("forgotPw");
+    let loginContainer = document.getElementById("login-container");
+    forgotContainer.removeAttribute("style");
+    loginContainer.style.display = "none";
+}
