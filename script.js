e3b1b042b88c1a274f07748eae209c43a61ce90c
cyberwerkstatt <seslija.sasha@gmail.com>
2023-01-26 21:46:35 +0100

    reset password updated

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 
 
+/**
+ * function for sending an Email for reset
+ */
+function sendMail(){
+    alert("test")
+}
 
 
-
+/**
+ * function for enterin summary.html as guest
+ */
 
 function guestLogin() {
     window.location.href = './summary.html'
