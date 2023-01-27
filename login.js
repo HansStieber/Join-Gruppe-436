eb52ff655e4f762f86b3142414160239e05f6868
Sasha SESLIJA <seslija.sasha@gmail.com>
2023-01-27 22:46:51 +0100

    login

--- login.js
+++ login.js
@@ -1,9 +1,20 @@
 
-function login(email, password) {
-    if (!localStorage.getItem("loggedInUser")) {
-        localStorage.setItem("loggedInUser", JSON.stringify({email: email, password: password}));
-        console.log("User logged in successfully.");
+function login() {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    let loggedInUser = users.find(user => user.email === email && user.password === password);
+  
+    if (loggedInUser) {
+      localStorage.setItem("loggedInUser", JSON.stringify(loggedInUser));
+      console.log("User logged in successfully.");
+      window.location.href = "./summary.html"
     } else {
-        console.log("User is already logged in.");
+      console.log("Invalid email or password. Login failed.");
     }
+    
 }
+
+
+
+  
