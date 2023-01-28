d0f14277a0f5f2fbed7b23a97a7092ffdb441513
PierreRudolph <Plettner@hotmail.de>
2023-01-28 13:43:48 +0100

    task.class und board.js angepasst,dummy tasks hinzugefügt. Drei svg namen umgeändert.

---
+++ summary.js
@@ -0,0 +1,9 @@
+function updateSummary() {
+    let tasksInBoardDiv = document.getElementById('tasks-in-board');
+    let tasksInProgressDiv = document.getElementById('tasks-in-progress');
+    let tasksInFeedbackDiv = docuemt.getElementById('tasks-in-feedback');
+    let urgentDiv = docuemnt.getElementById('urgent');
+    let urgentDateDiv = document.getElementById('urgent-date');
+    let toDoDiv = document.getElementById('to-do');
+    let doneDiv = document.getElementById('done');
+}
