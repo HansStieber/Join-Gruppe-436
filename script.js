86c231541fd5201f38063e0201fac93f57c5a9b3
HansStieber <hans.stieber@posteo.de>
2023-02-05 10:56:57 +0100

    commenting on script.js

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ let spliceCurrentContact;
 let indexOfCurrentContact = -1;
 let headerMenu;
 
+
+/*----------- GENERAL FUNCTIONS TO INITIALIZE PAGE -----------*/
+/**
+ * The function plays the loadBackend() and includeHTML() functions.
+ */
 async function load() {
     await loadBackend();
     await includeHTML();
 }
 
 
+/**
+ * The function loads all Data that is currently saved at the backend database.
+ */
 async function loadBackend() {
     await downloadFromServer();
     todos = JSON.parse(backend.getItem('todo')) || [];
@@ -62,9 +70,8 @@ function forgotPassword() {
 
 
 /*----------- GENERAL SHOW AND HIDE FUNCTIONS -----------*/
-
 /**
- * function to show New Task template
+ * the function shows the new Task Template.
  */
 async function showNewTaskCard() {
     await loadTemplateNewTask();
@@ -74,13 +81,14 @@ async function showNewTaskCard() {
     showShadowScreen('new-task-shadow-screen');
     slideInCard('new-task-overlay');
     showNewTaskCloseBtn();
-    if (document.URL.includes('contacts.html')) {
-        selectCurrentContact(contactToEditId);
-    } else {
-        loadAllOptions();
-    }
+    checkWhichIsCurrentPage();
 }
 
+
+/**
+ * The function renders the div where the new task template is located. It also runs the load() function again that runs the includeHTML()
+ * function which renders all templates.
+ */
 async function loadTemplateNewTask() {
     document.getElementById('new-task-overlay').innerHTML = `
     <div w3-include-html="./templates/new_task.html"></div>
@@ -88,30 +96,104 @@ async function loadTemplateNewTask() {
     await load();
 }
 
-//Von Hans
-function selectCurrentContact(i) {
-    if (assignments.some(a => a.firstName === contacts[i].firstName) && assignments.some(a => a.lastName === contacts[i].lastName)) {
-        loadAssignmentOptions();
-        for (let k = 0; k < assignments.length; k++) {
-            const assignment = assignments[k];
-            if (assignment.firstName === contacts[i].firstName) {
-                index = k;
-            }
-        }
-        assignContact(index);
-        spliceCurrentContact = false;
+
+/**
+ * The function checks on which page the new task card is opened. If there is a new task to create at the contacts.html, the current contact
+ * needs to be selected to already assign it to the task.
+ */
+function checkWhichIsCurrentPage() {
+    if (document.URL.includes('contacts.html')) {
+        selectCurrentContact(contactToEditId);
     } else {
-        assignments.push(contacts[i]);
-        loadAssignmentOptions();
-        if (indexOfCurrentContact == -1 && assignments.some(a => a.email == contacts[i].email)) {
-            indexOfCurrentContact = assignments.indexOf(contacts[i]);
-        }
-        let index = assignments.length - 1;
-        assignContact(index);
-        spliceCurrentContact = true;
+        loadAllOptions();
     }
 }
 
+
+/**
+ * The function checks if the the current contact is already part of the assignments array. If he is, the assignment options are loaded and
+ * the contact is assigned. If he is not, the contact is pushed to the assignments array before the assignment options are loaded and the
+ * contact is assigned.
+ * 
+ * @param {number} i - id of the selected contact at the contacts array
+ */
+function selectCurrentContact(i) {
+    if (currentContactisAlreadyAtAssignments(i)) {
+        loadAssignmentOptionsAndAssignContact(i);
+    } else {
+        pushContactAndLoadAssignmentsOptionsAndAssignContact(i);
+    }
+}
+
+
+/**
+ * The function checks if some contact at the assignments array has the same firstName and lastName as the current contact.
+ *
+ * @param {number} i - id of the current contact at the contacts array
+ */
+function currentContactisAlreadyAtAssignments(i) {
+    assignments.some(a => a.firstName === contacts[i].firstName) && assignments.some(a => a.lastName === contacts[i].lastName)
+}
+
+
+/**
+ * The function loads all assignment options and assigns the current contact. It also sets the spliceCurrentContact variable to false.
+ * 
+ * @param {number} i - id of the current contact at the contacts array 
+ */
+function loadAssignmentOptionsAndAssignContact(i) {
+    loadAssignmentOptions();
+    setOptionOneIndex(i);
+    assignContact(index);
+    spliceCurrentContact = false;
+}
+
+
+/**
+ * The function sets the index of the current contact at the assignments array. This is achieved by looping through the assignments array and
+ * checking if the firstName and lastName of current contact matches to current assignments conctact. If it matches the index is set. The index
+ * is used to run the assignContact() function with the index of the current contact at the assignments array.
+ * 
+ * @param {number} i - id of the current contact at the contacts array 
+ */
+function setOptionOneIndex(i) {
+    for (let k = 0; k < assignments.length; k++) {
+        const assignment = assignments[k];
+        if (assignment.firstName === contacts[i].firstName && assignment.lastName === contacts[i].lastName) {
+            index = k;
+        }
+    }
+}
+
+
+/**
+ * The function pushes the current contact to the assignments array. Then it loads all assignment options and assigns the current contact.
+ * It also sets the spliceCurrentContact variable to true.
+ * 
+ * @param {number} i - id of the current contact at the contacts array
+ */
+function pushContactAndLoadAssignmentsOptionsAndAssignContact(i) {
+    assignments.push(contacts[i]);
+        loadAssignmentOptions();
+        setOptionTwoIndex(i);
+        assignContact(index);
+        spliceCurrentContact = true;
+}
+
+
+/**
+ * The function sets the index of the current contact at the assignments array. This is achieved by looping through the assignments array and
+ * checking if the firstName and lastName of current contact matches to current assignments conctact. If it matches the index is set. The index
+ * is used to run the assignContact() function with the index of the current contact at the assignments array.
+ * 
+ * @param {number} i - id of the current contact at the contacts array 
+ */
+function setOptionTwoIndex(i) {
+    if (indexOfCurrentContact == -1 && assignments.some(a => a.email == contacts[i].email)) {
+        indexOfCurrentContact = assignments.indexOf(contacts[i]);
+    }
+    index = assignments.length - 1;
+}
 
 function removeCurrentContact() {
     if (spliceCurrentContact == true) {
