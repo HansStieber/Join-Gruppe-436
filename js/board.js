55eabbe2e41b10cf4c74531dd79d2d3ed05c578a
PierreRudolph <Plettner@hotmail.de>
2023-02-10 10:56:09 +0100

    responsivness detail-view

--- js/board.js
+++ js/board.js
@@ -14,7 +14,7 @@ async function initBoard() {
 
 window.addEventListener('resize', function () {
     if (location.href.includes('board.html') || location.href.includes('contacts.html')) {
-        hideNewTaskCard();
+        //hideNewTaskCard();
     }
 });
 
@@ -293,7 +293,11 @@ function generateHTMLDetailCard(idOfCard) {
     return /*html*/`
         <img src="../assets/img/close.svg" alt="closing-icon" class="icon-settings" onclick="closeDetailView()">
         <img src="../assets/img/left_arrow.svg" alt="left arrow" class="icon-settings d-none">
-        <div class="category-styling ${categoryBg}">${categoryTitel}</div>
+        <div class="detail-view-category">
+        <div class="category-styling ${categoryBg}">
+        ${categoryTitel}</div>
+        <img src="../assets/img/arrow-left-contacts.svg" alt="back-arrow" class="detail-view-arrow" onclick="closeDetailView()">
+</div>
         <div class="detail-view-title">${title}</div>
         <div>${description}</div>
         <div class="flex-center">
