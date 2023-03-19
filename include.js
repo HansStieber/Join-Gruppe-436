7f154efe77ba44be7f8564d0276f6c67b34b55ed
PierreRudolph <Plettner@hotmail.de>
2023-03-19 15:47:39 +0100

    add new include function

--- include.js
+++ include.js
@@ -3,7 +3,7 @@
  * This function get all HTML-Element, with Name '[w3-include-html]'.
  * Fetch them and add them to HTML.
  */
-async function includeHTML() {
+/*async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
         const element = includeElements[i];
@@ -15,5 +15,45 @@ async function includeHTML() {
             element.innerHTML = 'Page not found';
         }
     }
+}*/
+
+/**
+ *  This function get all HTML-Element, with Name 'include-html'.
+ * And load them into Html.
+ */
+function includeHTML() {
+    var elements = document.querySelectorAll('[include-html]');
+    var remainingElements = elements.length;
+    if (remainingElements === 0) {
+        allIncludedMsg();
+    } else {
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            var url = element.getAttribute('include-html');
+            if (url) {
+                var xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4) {
+                        if (xhr.status === 200) {
+                            element.innerHTML = xhr.responseText;
+                        }
+                        element.removeAttribute('include-html');
+                        remainingElements--;
+                        if (remainingElements === 0) {
+                            allIncludedMsg();
+                        } else {
+                            // Include remaining HTML files
+                            includeHTML();
+                        }
+                    }
+                };
+                xhr.open('GET', url, true);
+                xhr.send();
+            }
+        }
+    }
 }
 
+function allIncludedMsg() {
+    console.log('All HTML files have been included!');
+}
