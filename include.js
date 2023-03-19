69165018b266d36a5903dff12c58c1214b971d6a
PierreRudolph <Plettner@hotmail.de>
2023-03-19 16:46:17 +0100

    comment

--- include.js
+++ include.js
@@ -1,13 +1,13 @@
 
 /**
- * This function get all HTML-Element, with Name '[w3-include-html]'.
+ * This function get all HTML-Element, with Name '[include-html]'.
  * Fetch them and add them to HTML.
- */
+ *------------W3-Include---------*/
 /*async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
+    let includeElements = document.querySelectorAll('[include-html]');
     for (let i = 0; i < includeElements.length; i++) {
         const element = includeElements[i];
-        file = element.getAttribute("w3-include-html");
+        file = element.getAttribute("include-html");
         let resp = await fetch(file);
         if (resp.ok) {
             element.innerHTML = await resp.text();
@@ -21,39 +21,32 @@
  *  This function get all HTML-Element, with Name 'include-html'.
  * And load them into Html.
  */
-function includeHTML() {
-    var elements = document.querySelectorAll('[include-html]');
-    var remainingElements = elements.length;
-    if (remainingElements === 0) {
-        allIncludedMsg();
-    } else {
-        for (var i = 0; i < elements.length; i++) {
-            var element = elements[i];
-            var url = element.getAttribute('include-html');
-            if (url) {
-                var xhr = new XMLHttpRequest();
+
+
+async function includeHTML() {
+    const elements = document.querySelectorAll('[include-html]');
+    const promises = [];
+
+    for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        const url = element.getAttribute('include-html');
+        if (url) {
+            const promise = new Promise((resolve) => {
+                const xhr = new XMLHttpRequest();
                 xhr.onreadystatechange = function () {
-                    if (xhr.readyState === 4) {
-                        if (xhr.status === 200) {
-                            element.innerHTML = xhr.responseText;
-                        }
+                    if (xhr.readyState === 4 && xhr.status === 200) {
+                        element.innerHTML = xhr.responseText;
                         element.removeAttribute('include-html');
-                        remainingElements--;
-                        if (remainingElements === 0) {
-                            allIncludedMsg();
-                        } else {
-                            // Include remaining HTML files
-                            includeHTML();
-                        }
+                        resolve();
                     }
                 };
                 xhr.open('GET', url, true);
                 xhr.send();
-            }
+            });
+            promises.push(promise);
         }
     }
-}
 
-function allIncludedMsg() {
+    await Promise.all(promises);
     console.log('All HTML files have been included!');
 }
